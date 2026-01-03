92d5b9d83cee6722ddf1f75079cd8976cfa95a0a
yumyum-02 <yumyum0kome27@gmail.com>
2026-01-03 16:40:46 +0900

    ユーザー一覧ページ改修

--- src/admin.php
+++ src/admin.php
@@ -4,6 +4,26 @@ require './db.php';
 session_start();
 session_regenerate_id();
 
+if (!isset($_SESSION['user'])) {
+  header('Location: ./login.php');
+  exit();
+}
+
+if (isset($_POST['logout'])) {
+
+  if (empty($_SESSION['logout_token']) || ($_SESSION['logout_token'] !== $_POST['logout_token'])) exit('不正な投稿です');
+  if (isset($_SESSION['logout_token'])) unset($_SESSION['logout_token']);
+  if (isset($_POST['logout_token'])) unset($_POST['logout_token']);
+
+  $_SESSION = array();
+  if (isset($_COOKIE["PHPSESSID"])) setcookie("PHPSESSID", '', time() - 1800, '/');
+  session_destroy();
+
+  $msg = urlencode("ログアウトしました。");
+  header('Location: ./login.php?msg=' . $msg);
+  exit();
+}
+
 $pdo = connectDb();
 $sql = ('
     SELECT login_id, name
