59d1431f821c9bca43ea87eced162635145fd903
yumyum-02 <yumyum0kome27@gmail.com>
2026-02-27 21:55:49 +0900

    public/logout.phpを作成して処理を分ける

---
+++ public/logout.php
@@ -0,0 +1,12 @@
+<?php
+require_once '../src/bootstrap.php';
+
+if (isset($_POST['logout'])) {
+  // CSRFトークン検証
+  if (!verifyCsrfToken($_POST['csrf_token'] ?? '')) {
+    exit('不正な投稿です');
+  }
+
+  // ログアウト実行（この中でリダイレクト+exitされる）
+  executeLogout();
+}
--- src/functions/logout.php
+++ src/functions/logout.php
@@ -1,15 +1,5 @@
 <?php
-if (isset($_POST['logout'])) {
-  // CSRFトークン検証
-  if (!verifyCsrfToken($_POST['csrf_token'] ?? '')) {
-    exit('不正な投稿です');
-  }
-
-  // ログアウト実行（この中でリダイレクト+exitされる）
-  executeLogout();
-}
-
-// ログアウト処理を実行
+// ログアウト処理
 function executeLogout(): void
 {
   $_SESSION = [];
