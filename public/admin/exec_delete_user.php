b2d36f84524742d935cc79c4597056170ececccf
ichikawa <ichikawa@kokochie.jp>
2026-03-27 10:35:16 +0900

    ユーザー削除処理修正

--- public/admin/exec_delete_user.php
+++ public/admin/exec_delete_user.php
@@ -11,6 +11,7 @@ requireValidCsrfToken();
 $user_id = filter_input(INPUT_POST, 'id', FILTER_VALIDATE_INT);
 // filter_input PHPの組み込み関数 外部からの入力データを安全に取得・検証
 // filter_input(タイプ, 変数名, フィルタ, オプション)
+// データの出所がPOSTであることを確認する
 // POSTデータから id パラメータを取得
 // FILTER_VALIDATE_INT で整数としてバリデーション（悪意のあるSQL文を注入するのを防ぐため整数だけ受け付ける）
 // 有効な整数なら その値、無効なら false を返す
@@ -20,14 +21,14 @@ if (!$user_id){
   redirect('./admin.php');
 }
 
+// ログイン中のユーザーは削除不可
+if (isset($_SESSION['user']['id']) && $user_id === (int)$_SESSION['user']['id']) {
+  $_SESSION['error'] = 'ログイン中のユーザー情報は削除できません';
+  redirect('./admin.php');
+}
+
 //　削除処理
 try {
-  // ログイン中のユーザーは削除不可
-  if (isset($_SESSION['user']['id']) && $user_id === (int)$_SESSION['user']['id']) {
-    $_SESSION['error'] = 'ログイン中のユーザー情報は削除できません';
-    redirect('./admin.php');
-  }
-
   // 削除実行
   $deleted_count = deleteUserById($user_id);
 
