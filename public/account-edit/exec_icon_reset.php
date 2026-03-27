603c76fcf469ca04bad7145d86a0975a241645b5
ichikawa <ichikawa@kokochie.jp>
2026-03-27 11:05:22 +0900

    アイコンデフォルト機能修正

--- public/account-edit/exec_icon_reset.php
+++ public/account-edit/exec_icon_reset.php
@@ -11,11 +11,16 @@ requireLogin('../login.php');
 requireValidCsrfToken();
 
 try {
+  $userId = $_SESSION['user']['id'];
+
+  // プレビュー用ファイル（{id}_temp.jpg など）を削除
+  deleteTempIconFile($userId);
   // アイコンをリセット（該当ユーザーの画像を全削除、DB NULL）
-  resetIcon($_SESSION['user']['id']);
+  resetIcon($userId);
 
   // セッションのユーザー情報を更新
   $_SESSION['user']['icon'] = null;
+  unset($_SESSION['temp_icon']);
 
   // 成功時は account.php にリダイレクト
   redirect('../admin/account.php');
